4297a0be1ec484e6783004564d31f47fe5d468fc
kimmiehoang <tienhoangngockim@gmail.com>
2024-04-05 14:20:23 +0700

    implementing websocket (finished)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,3 @@
-// Import thư viện Express
 import express from 'express';
 import { json } from 'express';
 import cors from 'cors';
@@ -6,6 +5,9 @@ import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
 import userRouter from './router/userRouter.js';
 import mongoose from 'mongoose';
+import mqtt from 'mqtt';
+import http from 'http';
+import { Server } from 'socket.io';
 
 mongoose.connect('mongodb://127.0.0.1:27017/smartHome', {
   useNewUrlParser: true,
@@ -24,8 +26,42 @@ const port = 5000;
 app.use(json());
 app.use(cors());
 
+/////////////////////////
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+const client = mqtt.connect('mqtt://io.adafruit.com', {
+  username: 'tienhoang',
+  password: '',
+});
+
+client.on('connect', () => {
+  console.log('Connected to MQTT broker');
+  client.subscribe('tienhoang/feeds/bbc-led');
+  client.subscribe('tienhoang/feeds/bbc-fan');
+});
+
+io.on('connection', (socket) => {
+  console.log('A client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+client.on('message', (topic, message) => {
+  console.log('Received message from Adafruit:', message.toString());
+  const data = parseInt(message);
+  io.sockets.emit('dataFromServer', { topic: topic, data: data });
+});
+/////////////////////////
+
 app.use('/users', userRouter);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
